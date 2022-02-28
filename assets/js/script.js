97245031b97ba58519640f9b2bff4ffb27c45234
Elexis Blash <elexisblash@gmail.com>
2022-02-28 10:21:23 -0500

    Added skeleton for questions and quiz

---
+++ assets/js/script.js
@@ -0,0 +1,85 @@
+
+function startQuiz() {
+
+    var output = [];
+    var choices;
+
+    for (var i = 0; i < questions.length; i++) {
+        choices = [];
+
+        for (letter in questions[i].choices) {
+            choices.push(
+                '<label>' + '<input type="radio" name="question' + i + '" value="' + letter + '">'
+                + letter + ': '
+                + questions[i].choices[letter]
+                + '</label>'
+            );      
+        }
+        output.push(
+            '<div class="question">' + questions[i].question + '</div>'
+            + '<div class="answers">' + choices.join('') + '</div>'
+        );
+    }
+
+    quizContainer.innerHTML = output.join('');
+
+}
+
+var startButton = document.getElementById('start');
+var quizContainer = document.getElementById('quiz');
+var resultsContainer = document.getElementById('results');
+
+var questions = [
+    {
+        question: "1",
+        choices: {
+            a: "",
+            b: "",
+            c: "",
+            d: "",
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "2",
+        choices: {
+            a: "",
+            b: "",
+            c: "",
+            d: "",
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "3",
+        choices: {
+            a: "",
+            b: "",
+            c: "",
+            d: "",
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "4",
+        choices: {
+            a: "",
+            b: "",
+            c: "",
+            d: "",
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "5",
+        choices: {
+            a: "",
+            b: "",
+            c: "",
+            d: "",
+        },
+        correctAnswer: "c"
+    }
+];
+
+startButton.addEventListener('click', startQuiz);
