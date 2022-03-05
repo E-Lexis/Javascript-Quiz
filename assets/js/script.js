d4ff1c8a0163fe31a580cf126d5e1df9def62b9a
Elexis Blash <elexisblash@gmail.com>
2022-03-05 10:14:11 -0500

    Added right/wrong feedback to quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,56 +48,83 @@ var questions = [
     }
 ];
 
-let indexNumber = 0
+var indexNumber = 0;
+var playerScore = 0;
 
 function loadQuestions(index) {
 
     const currentQuestion = questions[index];
 
     document.getElementById("display-question").innerHTML = currentQuestion.question;
-    document.getElementById("button1").value = currentQuestion.answer1;
-    document.getElementById("button2").value = currentQuestion.answer2;
-    document.getElementById("button3").value = currentQuestion.answer3;
-    document.getElementById("button4").value = currentQuestion.answer4;
+    document.getElementById("button1-label").textContent = currentQuestion.answer1;
+    document.getElementById("button2-label").textContent = currentQuestion.answer2;
+    document.getElementById("button3-label").textContent = currentQuestion.answer3;
+    document.getElementById("button4-label").textContent = currentQuestion.answer4;
+
+    var options = document.getElementsByClassName("radio");
+
+    for (var i = 0; i < options.length; i++) {
+        options[i].addEventListener('change', handleNextQuestion);
+    }
 }
 
 function checkAnswer() {
     const currentQuestion = questions[indexNumber];
     const currentAnswer = currentQuestion.correctAnswer;
+    const choices = document.getElementsByName("choice");
     var correctChoice = null;
-    var choices = document.getElementsByName("choice");
+    
 
     choices.forEach((choice) => {
+
         if (choice.value === currentAnswer) {
             correctChoice = choice.labels[0].id
             }
         })
 
-
-        choices.forEach((choice) => {
-            if (choice.clicked == true && choice.value == currentAnswer) {
-                document.getElementById(correctChoice).style.backgroundColor = "green";
-                indexNumber++;
-            }
-
-            else if (choice.clicked == true && choice.value != currentAnswer) {
-                const wrongLabelId = choice.labels[0].id;
-                document.getElementById(wrongLabelId).style.backgroundColor = "red";
-                document.getElementById(correctChoice).style.backgroundColor = "green";
-            }
-        })
+    choices.forEach((choice) => {
+        if (choice.checked == true && choice.value == currentAnswer) {
+            document.getElementById(correctChoice).style.backgroundColor = "green";
+            indexNumber++;
+            playerScore++;
+        }
+         else if (choice.checked == true && choice.value != currentAnswer) {
+             const wrongLabelId = choice.labels[0].id;
+             document.getElementById(wrongLabelId).style.backgroundColor = "red";
+             document.getElementById(correctChoice).style.backgroundColor = "green";
+             indexNumber++;
+        }
+    })
           
 }
 
 function handleNextQuestion() {
-
     checkAnswer();
-    
-    if (indexNumber <= questions.length) {
-        loadQuestions(indexNumber);
-    } else {
-        handleEndGame();
+    unCheckRadioButtons();
+
+    setTimeout(() => {
+        if (indexNumber <= 9) {
+            loadQuestions(indexNumber)
+        }
+        else {
+            handleEndGame()
+        }
+        resetChoices()
+    }, 1000);
+}
+
+function unCheckRadioButtons() {
+    const choices = document.getElementsByName("choice");
+    for (let i = 0; i < choices.length; i++) {
+        choices[i].checked = false;
     }
+}
+
+function resetChoices() {
+    const choices = document.getElementsByName("choice");
+    choices.forEach((choice) => {
+        document.getElementById(choice.labels[0].id).style.backgroundColor = ""
+    })
 }
 
 //Start Quiz clicked
